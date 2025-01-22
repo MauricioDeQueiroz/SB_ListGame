115f2e3722c51d5f2849874890ec59111ce06712
MauricioDeQueiroz <mauriciodequeirozz@gmail.com>
2025-01-22 16:00:44 -0300

    Properties, Game, ORM

--- src/main/java/com/MGames/ProjectSbApplication.java
+++ src/main/java/com/MGames/ProjectSbApplication.java
@@ -1,4 +1,4 @@
-package com.mauriciodequeiroz.project_sb;
+package com.MGames;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
